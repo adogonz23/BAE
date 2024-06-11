4d87d2638fc0710d3ab03dce56a4430852918a2d
adogonz23 <adogonzalez75@gmail.com>
2024-06-11 15:51:54 +0100

    revisado

--- mongoDb/src/main/java/ies/puerto/mongodb/controlador/CocheControlador.java
+++ mongoDb/src/main/java/ies/puerto/mongodb/controlador/CocheControlador.java
@@ -3,8 +3,10 @@ package ies.puerto.mongodb.controlador;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.origin.Origin;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
